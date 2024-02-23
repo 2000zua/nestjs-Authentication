824961485d8c29858ce2cb37d061554570e22b19
Angelo Abreu Zua <Gildozua1@hotmail.com>
2024-02-23 22:49:46 +0300

    nuhi

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from './entities/user.entity';
+import { Prisma } from '@prisma/client';
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  //contrutor
+  constructor(private readonly prisma: PrismaService){}
+
+  async create(createUserDto: CreateUserDto): Promise<User>{
+    const data: Prisma.UserCreateInput = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+    const createdUser = await this.prisma.user.create({data});
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+  
+  findByEmaik(email: string){
+    return this.prisma.user.findUnique({where:{email}})
   }
 
   findAll() {
